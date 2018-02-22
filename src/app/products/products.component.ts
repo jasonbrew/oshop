2471689e0b72863f85aee4c77fb9e58b675ee08b
Jason Brewer <jason.brewer@rentacenter.com>
2018-02-21 21:47:26 -0600

    Lecture 322

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -20,15 +20,18 @@ export class ProductsComponent {
     productService: ProductService, 
     categoryService: CategoryService) { 
 
-    productService.getAll().subscribe(products => this.products = products);
-    this.categories$ = categoryService.getAll();
-
-    route.queryParamMap.subscribe(params => {
-      this.category = params.get('category');
+    productService.getAll().subscribe(products => {
+      this.products = products
       
-      this.filteredProducts = (this.category) ?
-        this.products.filter(p => p.category === this.category) :
-        this.products;
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+        
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
     });
+
+    this.categories$ = categoryService.getAll();
   }
 }
